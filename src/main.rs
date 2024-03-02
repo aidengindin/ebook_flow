e769f0af341b5832d6946dfd3c1572d8412d882b
Aiden Gindin <aiden@thegindins.com>
2024-03-02 15:05:16 -0500

    basic code to scan working directory for ebook files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
-use std::fs;
+use std::{fs, collections::HashSet};
+
+#[derive(Eq, Hash, PartialEq)]
+struct BookFile {
+    path: String,
+    file_type: FileType,
+}
+
+impl BookFile {
+    fn new(path: String, file_type: FileType) -> BookFile {
+        BookFile { path, file_type }
+    }
+}
+
+#[derive(Eq, Hash, PartialEq)]
+enum FileType {
+    Acsm,
+    Epub,
+    Azw,
+}
 
 fn main() {
     let paths = fs::read_dir("./").unwrap();
+    let mut acsm_files = HashSet::new();
+    let mut ebook_files = HashSet::new();
 
     for path in paths {
-        println!("{}", path.unwrap().path().display())
+        let path = path.unwrap().path().display().to_string();
+        if path.contains(".acsm") {
+            acsm_files.insert(BookFile::new(path, FileType::Acsm));
+        } else if path.contains(".epub") {
+            ebook_files.insert(BookFile::new(path, FileType::Epub));
+        } else if path.contains(".azw") {
+            ebook_files.insert(BookFile::new(path, FileType::Azw));
+        }
     }
+
+    println!("{} acsm files", acsm_files.len());
+    println!("{} ebook files", ebook_files.len());
 }
