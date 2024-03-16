d75bbe8ba095c3701a9f27a24bb07aa51231a8b2
Aiden Gindin <aiden@thegindins.com>
2024-03-16 16:36:21 -0400

    add basic config struct

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,21 @@
 use serde::Deserialize;
 use std::error::Error;
+use url::Url;
 
 #[derive(Deserialize)]
 pub struct Config {
-
+    file_path: String,
+    calibre_url: Url,
+    acsm_conversion_timeout: u32,
+    calibre_upload_timeout: u32,
 }
 
 pub fn load_config() -> Result<Config, Box<dyn Error>> {
-    Ok(Config {})
+    Ok(Config {
+        file_path: "".to_string(),
+        calibre_url: Url::parse("https://aidengindin.com")?,
+        acsm_conversion_timeout: 48,
+        calibre_upload_timeout: 48,
+    })
 }
 
